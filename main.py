584164b86eaed342f19e2a56558511789912cd0f
YashGoswamiBIAS <100066481+YashGoswamiBIAS@users.noreply.github.com>
2022-10-03 13:58:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+
+CORS(app)
 
 import firebase_admin
 from firebase_admin import credentials
@@ -16,6 +19,7 @@ ref = db.reference("/")
 
 
 @app.route("/api")
+@cross_origin
 def api():
     return ref.get()
 
